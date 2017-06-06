273a253d0743fdf2c3a4b6aa2a9c1ae2774ea970
C-Benjamin <benjamin.cuny@viacesi.fr>
2017-06-06 16:38:27 +0200

    phase 4

---
+++ helloworldmaven/helloworldmvc.main/src/main/java/helloworldmvc/main/Main.java
@@ -0,0 +1,18 @@
+package helloworldmvc.main;
+
+import helloworldmvc.Contract.IModel;
+import helloworldmvc.Contract.IView;
+import helloworldmvc.controller.Controller;
+import helloworldmvc.model.Model;
+import helloworldmvc.view.View;
+
+public class Main {
+
+	public static void main(final String[] args) {
+		final IModel model = new Model();
+		final IView view = new View();
+		final Controller controller = new Controller(view, model);
+
+	}
+
+}
